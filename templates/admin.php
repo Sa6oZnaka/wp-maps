97a7aee18aadb239eb5db02160e1c8e48c44c5b5
Alexandar Karapenev <treildr@gmail.com>
2021-07-03 11:14:59 +0300

    Added map page

---
+++ templates/admin.php
@@ -0,0 +1,52 @@
+<h1> Google maps </h1>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Add Map</title>
+
+    <style>
+	  #map {
+		  height: 400px;
+		  /* The height is 400 pixels */
+		  width: 100%;
+		  /* The width is the width of the web page */
+		}
+	</style>
+    
+    <script>
+    	// Initialize and add the map
+		const cords = [ { lat: -25.344, lng: 131.036 }, { lat: -20.344, lng: 131.036 } ]
+		
+		function initMap() {
+
+		  const map = new google.maps.Map(document.getElementById("map"), {
+			zoom: 4,
+			center: cords[0],
+		  });
+		  // The marker, positioned at Uluru
+		  for(let i = 0; i < cords.length; i ++){
+			  new google.maps.Marker({
+				position: cords[i],
+				map: map,
+			  });
+		  }		  
+		  
+		}
+    </script>
+    
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+
+    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTe22xnL6uMAujuHn86a04WDcj2lISKCk&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
+    
+    
+  </body>
+</html>
--- wp-maps.php
+++ wp-maps.php
@@ -15,6 +15,25 @@ class MapPlugin {
 		add_action('init' , array( $this, 'custom_post_type'));
 	}
 	
+	function register(){
+		// admin_enqueue_scripts => backend, wp_enqueue_scripts -> frontend
+		add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+		
+		add_action( 'admin_menu' , array( $this, 'add_admin_pages' ));
+	}
+	
+	function add_admin_pages(){
+		add_menu_page( 'Map plugin' , 'Map' , 'manage_options', 'wp_map_plugin', array( $this , 'admin_index' ), 'dashicons-store' , 110 );
+	}
+	
+	function admin_index(){
+		// require template
+		
+		require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
+		
+	}
+	
+	
 	function activate(){
 		// generate a CPT
 		$this -> custom_post_type();
@@ -35,12 +54,18 @@ class MapPlugin {
 	function custom_post_type(){
 		register_post_type('location', ['public' => true, 'label' => 'Locations']);
 	}
-
+	
+	function enqueue () {
+		// enque all our scrips
+		wp_enqueue_style( 'mypluginstyle', plugins_url('/assets/styles.css', __FILE__));
+		wp_enqueue_script( 'mypluginscript', plugins_url('/assets/script.js', __FILE__));
+	}
 
 }
 
 if(class_exists('MapPlugin')){
 	$mapPlugin = new MapPlugin();
+	$mapPlugin -> register();
 }
 
 // activation
