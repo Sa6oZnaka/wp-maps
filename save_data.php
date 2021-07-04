9919db500ceb06051eca1d9beafe9cf4f4d845f4
Alexandar Karapenev <treildr@gmail.com>
2021-07-04 19:22:20 +0300

    App now works with external API

--- save_data.php
+++ save_data.php
@@ -5,15 +5,16 @@ function save_to_database(){
 	
 	global $wpdb;
 		
-	$jsonData = file_get_contents('http://192.168.1.3:3000/api/points');
+	$jsonData = file_get_contents('http://api.openweathermap.org/data/2.5/weather&appid=6d96731f08d3514a13ac8dd5c5a3f3d3');
 	$jsonDecode = json_decode($jsonData, true);
 	
 	$table_name = $wpdb -> prefix . "pins";
 
 	foreach($jsonDecode as $key => $value){
+		$coords = $value['coord'];
 		$wpdb->insert($table_name, array(
-			'latitude' => $value['latitude'],
-			'longitude' => $value['longitude']
+			'latitude' => $coords['lat'],
+			'longitude' => $coords['lon']
 		));
 	}
 	
